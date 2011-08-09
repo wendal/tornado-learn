625f29fdb47b71bbdb9b94a31c68b3665fe483e6
wendal <zhencheng.chen@danoonetworks.com>
2011-08-09 21:16:52 +0800

    simple form -- get params

---
+++ day_1/forms.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3.2
+
+import tornado.ioloop
+import tornado.web
+import time
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('<html><body><form action="/" method="post">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_argument("message"))
+        
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
